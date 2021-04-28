b959044e2ed6cdc96a834350a646a06f147c1e67
Artur <info@aigroupusa.com>
2021-04-28 15:48:17 -0400

    commit tree 0f84918ee99fe2b1ba425ea4c549ff65271e7180

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,10 +17,11 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/registrations' do
+    
     @user = User.new(name: params["name"], email: params["email"], password: params["password"])
     @user.save
     session[:user_id] = @user.id
-
+    
     redirect '/users/home'
   end
 
@@ -30,7 +31,7 @@ class ApplicationController < Sinatra::Base
     erb :'sessions/login'
   end
 
-  post '/sessions' do
+  post '/sessions' do 
     @user = User.find_by(email: params[:email], password: params[:password])
     if @user
       session[:user_id] = @user.id
